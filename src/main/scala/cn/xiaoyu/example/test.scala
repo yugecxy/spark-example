609ce41bd1d02d47258274000c2c264c0911b14d
chenxiaoyu <2580246520@qq.com>
2019-10-17 08:56:19 +0800

    10.17

--- src/main/scala/cn/xiaoyu/example/test.scala
+++ src/main/scala/cn/xiaoyu/example/test.scala
@@ -1,32 +1,56 @@
 package cn.xiaoyu.example
 
-import org.apache.spark.sql.{Column, Row, SparkSession}
+import org.apache.spark.sql.{Column, Encoder, Row, SparkSession}
 import org.apache.spark.sql.functions._
 import java.io.FileWriter
 import java.sql.Timestamp
 import java.text.{DecimalFormat, SimpleDateFormat}
-import java.util.{Calendar, GregorianCalendar}
-import java.util.Date
+import java.util
+import java.util.{Base64, Calendar, Date, GregorianCalendar}
 
+import breeze.util.Encoder
 import org.apache.commons.lang3.time.{DateUtils, FastDateFormat}
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types._
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.{LinearSeq, SortedSet}
+import scala.collection.immutable._
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.util.Random
+
+object t {
+  def apply(a: Int): Int = 9
+
+
+}
+
+case class m(a: String)
+
 
 object test {
 
-  val spark = SparkSession.builder()
-    .master("local[1]")
-    .config("spark.shuffle.sort.bypassMergeThreshold", "1")
+  lazy val spark = SparkSession.builder()
+    .master("local[3]")
     .getOrCreate()
-//
-  spark.sparkContext.setLogLevel("WARN")
+
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-    val inputDF = spark.read.option("header", "true").csv("/Users/chenxiaoyu/Desktop/化工/化工source3/目录-表格 1.csv")
-    println(inputDF.select("产品名称").as[String].collect().mkString("\",\""))
-  }
+    val rdd1 = spark.sparkContext.makeRDD(Seq(
+      ("a", 3),
+      ("b", 4),
+      ("b", 6)
+    ))
 
+    val rdd2 = spark.sparkContext.makeRDD(Seq(
+      ("a", "yun"),
+      ("b", "yu")
+    ))
+
+    val r1 = rdd1.reduceByKey((a, b) => a + b)
+    val r2 = rdd2.reduceByKey((a, b) => a + b)
+    r2.join(r1,7).foreach(println(_))
+    Thread.sleep(1000000)
+  }
 }
 
