0b9b8b54b9b04c9860ec2205a441078ca7d1ebab
陈小雨 <chenxiaoyu@test.com>
2020-06-04 17:52:11 +0800

    2019 06 04

--- src/main/scala/cn/xiaoyu/example/test.scala
+++ src/main/scala/cn/xiaoyu/example/test.scala
@@ -2,7 +2,7 @@ package cn.xiaoyu.example
 
 import org.apache.spark.{Partitioner, RangePartitioner}
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
-import org.apache.spark.sql.{Column, Row, SaveMode, SparkSession}
+import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.json4s._
@@ -16,45 +16,47 @@ class MyPartitioner(partitions: Int) extends Partitioner {
 
 
 object JsonTest {
-  lazy val spark = SparkSession.builder().master("local[4]")
+  lazy val spark = SparkSession.builder().master("local[6]")
     .config("spark.sql.files.maxPartitionBytes", 5 * 1024 * 1024)
     .config("parquet.block.size", 16 * 1024 * 1024)
     .getOrCreate()
 
-  def save() = {
-    val conUrl = s"jdbc:mysql://rm-6wep82uuvdggcd4k1po.mysql.japan.rds.aliyuncs.com:3306/test?characterEncoding=utf-8&zeroDateTimeBehavior=CONVERT_TO_NULL&useSSL=false&connectTimeout=10000&serverTimezone=UTC"
-    val df = spark.read.parquet("/Users/chenxiaoyu/Desktop/test.snappy.parquet")
-    println(df.count())
-    println("begin to save...")
-
-    df.write
-      .mode(SaveMode.Overwrite)
-      .format("jdbc")
-      .options(Map(
-        "url" -> conUrl,
-        "dbtable" -> "1w_one_part_test",
-        "driver" -> "com.mysql.cj.jdbc.Driver",
-        "user" -> "chenxiaoyu",
-        "password" -> "chenxiaoyu123"
-      )).save()
-    println("end to save...")
+  /**
+    * S_VAL_PB_NEW,OPER_REV_TTM
+    */
+  def countDistribution(df: DataFrame, key: String) = {
+    df.groupBy(key).agg(count(lit(1)).as("cnt")).orderBy(-col("cnt"))
   }
 
-
-  def main(args: Array[String]) {
-    var df1 = spark.read.parquet("/Users/chenxiaoyu/Desktop/test_fast")
-    var df2 = spark.read.parquet("/Users/chenxiaoyu/Desktop/test_fast_join2")
+  def usePartitioner(df: DataFrame): DataFrame = {
+    val schema = df.schema
     val patitioner = new MyPartitioner(200)
-
-    val rdd1 = df1.rdd.map(x => (x.getAs[Any]("idx_name"), x))
+    val rdd = df.rdd.map(x => (x.getAs[Any]("idx_name"), x))
       .partitionBy(patitioner)
       .map(_._2)
+    spark.createDataFrame(rdd, schema)
+  }
+
+  def main(args: Array[String]) {
+    //    .filter(col("idx_name") === "S_VAL_MV")
+    var df1 = spark.read.parquet("/Users/chenxiaoyu/Desktop/port=1_back")
+      .filter(col("idx_name") === "S_VAL_PB_NEW" || col("idx_name") === "OPER_REV_TTM")
+    //    df1 = usePartitioner(df1)
+    //    countDistribution(df1, "idx_name").show()
+    //    countDistribution(df1,"idx_name").show(1000)
+    var df2 = spark.read.parquet("/Users/chenxiaoyu/Desktop/test_fast_join2")
+    //    val patitioner = new MyPartitioner(200)
+    //
+    //    val rdd1 = df1.rdd.map(x => (x.getAs[Any]("idx_name"), x))
+    //      .partitionBy(patitioner)
+    //      .map(_._2)
 
     df1.join(broadcast(df2), df1("idx_name") === df2("t"), "right")
+      //      .show()
       .write.mode(SaveMode.Overwrite).parquet("/Users/chenxiaoyu/Desktop/test_res")
-    //    Thread.sleep(1000000)
-
     //    df.repartition(1).write.mode(SaveMode.Overwrite).parquet("/Users/chenxiaoyu/Desktop/test_fast")
+
+    Thread.sleep(10000000)
   }
 }
 
