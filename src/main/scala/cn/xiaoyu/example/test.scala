5d29ad3bf47494b37109ab45d9732b1908b46131
陈小雨 <chenxiaoyu@test.com>
2020-06-04 15:33:51 +0800

    2019 06 04

--- src/main/scala/cn/xiaoyu/example/test.scala
+++ src/main/scala/cn/xiaoyu/example/test.scala
@@ -1,32 +1,60 @@
 package cn.xiaoyu.example
 
-import org.apache.spark.sql.{Column, Row, SparkSession}
+import org.apache.spark.{Partitioner, RangePartitioner}
+import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
+import org.apache.spark.sql.{Column, Row, SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
-import java.io.FileWriter
-import java.sql.Timestamp
-import java.text.{DecimalFormat, SimpleDateFormat}
-import java.util.{Calendar, GregorianCalendar}
-import java.util.Date
+import org.apache.spark.sql.types._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
-import org.apache.commons.lang3.time.{DateUtils, FastDateFormat}
+class MyPartitioner(partitions: Int) extends Partitioner {
+  override def numPartitions: Int = partitions
 
-import scala.collection.mutable.ListBuffer
-import scala.util.Random
+  override def getPartition(key: Any): Int = 0
+}
 
-object test {
 
-  val spark = SparkSession.builder()
-    .master("local[1]")
-    .config("spark.shuffle.sort.bypassMergeThreshold", "1")
+object JsonTest {
+  lazy val spark = SparkSession.builder().master("local[4]")
+    .config("spark.sql.files.maxPartitionBytes", 5 * 1024 * 1024)
+    .config("parquet.block.size", 16 * 1024 * 1024)
     .getOrCreate()
-//
-  spark.sparkContext.setLogLevel("WARN")
-  import spark.implicits._
 
-  def main(args: Array[String]): Unit = {
-    val inputDF = spark.read.option("header", "true").csv("/Users/chenxiaoyu/Desktop/化工/化工source3/目录-表格 1.csv")
-    println(inputDF.select("产品名称").as[String].collect().mkString("\",\""))
+  def save() = {
+    val conUrl = s"jdbc:mysql://rm-6wep82uuvdggcd4k1po.mysql.japan.rds.aliyuncs.com:3306/test?characterEncoding=utf-8&zeroDateTimeBehavior=CONVERT_TO_NULL&useSSL=false&connectTimeout=10000&serverTimezone=UTC"
+    val df = spark.read.parquet("/Users/chenxiaoyu/Desktop/test.snappy.parquet")
+    println(df.count())
+    println("begin to save...")
+
+    df.write
+      .mode(SaveMode.Overwrite)
+      .format("jdbc")
+      .options(Map(
+        "url" -> conUrl,
+        "dbtable" -> "1w_one_part_test",
+        "driver" -> "com.mysql.cj.jdbc.Driver",
+        "user" -> "chenxiaoyu",
+        "password" -> "chenxiaoyu123"
+      )).save()
+    println("end to save...")
   }
 
+
+  def main(args: Array[String]) {
+    var df1 = spark.read.parquet("/Users/chenxiaoyu/Desktop/test_fast")
+    var df2 = spark.read.parquet("/Users/chenxiaoyu/Desktop/test_fast_join2")
+    val patitioner = new MyPartitioner(200)
+
+    val rdd1 = df1.rdd.map(x => (x.getAs[Any]("idx_name"), x))
+      .partitionBy(patitioner)
+      .map(_._2)
+
+    df1.join(broadcast(df2), df1("idx_name") === df2("t"), "right")
+      .write.mode(SaveMode.Overwrite).parquet("/Users/chenxiaoyu/Desktop/test_res")
+    //    Thread.sleep(1000000)
+
+    //    df.repartition(1).write.mode(SaveMode.Overwrite).parquet("/Users/chenxiaoyu/Desktop/test_fast")
+  }
 }
 
